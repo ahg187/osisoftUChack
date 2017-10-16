351055b39d53cc71ff805f080d1c72b9f4a74b95
hacker22@osiproghack.int <hacker22@osiproghack.int>
2017-10-16 18:43:00 +0100

    anomaly detector implemented

---
+++ anomaly_detector.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 14:24:12 2017
+
+@author: hacker22
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import mahalanobis
+import hdbscan
+import matplotlib.pyplot as plt
+from matplotlib import dates
+plt.style.use('ggplot')
+import datetime as dt
+import seaborn as sns
+
+
+def get_outliers(df, plot=False):
+
+    # drop columns if values are constant
+    # drop columns if too little non-nan values in column
+
+    cols_delete = ['location']
+    for i, col in enumerate(df.columns):
+        if col == 'location':
+            continue
+        else:
+            vals = df.iloc[:,i].values
+            ratio_nan = np.sum(np.isnan(vals)) / len(vals)
+            if np.nanstd(vals) == 0.0 or ratio_nan > 0.5:
+                cols_delete.append(col)
+
+    cols = df.columns.values[~np.in1d(df.columns.values, cols_delete)]
+    location = df.location.unique()[0]
+    data = df.loc[df.location==location, cols].dropna()
+    time_idx = data.index
+    data = data.values
+    scaler = StandardScaler()
+    data_scl = scaler.fit_transform(X=data)
+
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=15).fit(data_scl)
+
+    #sns.distplot(clusterer.outlier_scores_[np.isfinite(clusterer.outlier_scores_)], rug=True)
+
+    threshold = pd.Series(clusterer.outlier_scores_).quantile(0.85)
+    outliers_idx = np.where(clusterer.outlier_scores_ > threshold)[0]
+
+    outliers = np.zeros((data.shape[0],1))
+    outliers[outliers_idx] = 1
+
+    outliers = pd.DataFrame(data=outliers, index=time_idx, columns=[location + ' outlier'])
+
+    if plot:
+
+        times = np.zeros(data.shape)
+        for i in range(times.shape[1]):
+            times[:,i] = time_idx
+
+        plt.scatter(times.T, data.T,  s=50, linewidth=0, c='gray', alpha=0.25)
+        plt.scatter(times[outliers_idx,:].T, data[outliers_idx,:].T, s=50, linewidth=0, c='red', alpha=0.5)
+
+
+    return outliers
+
